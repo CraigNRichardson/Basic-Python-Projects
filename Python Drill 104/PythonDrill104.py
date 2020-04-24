1b777e841fc3fdb98ab4c7f0fe9b37d8d35c5300
Craig Richardson <craig.n.richardson@gmail.com>
2020-04-23 23:38:36 -0600

    Update PythonDrill104

--- Python Drill 104/PythonDrill104.py
+++ Python Drill 104/PythonDrill104.py
@@ -5,7 +5,7 @@ import fnmatch
 fileList = ('information.docx','Hello.txt','myImage.png', \
             'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
 
-conn = sqlite3.connect('104.db')
+conn = sqlite3.connect('104b.db')
 with conn:
         cur = conn.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS tbl_textFiles( \
@@ -13,22 +13,12 @@ with conn:
                     filename TEXT \
                     )")
 
-def sql_insert(conn, file):
-    for file in fileList:
-        if fnmatch.fnmatch(file, '*.txt'):
-            with conn:
-                cur = conn.cursor()
-                cur.execute("INSERT INTO tbl_textFiles VALUES (?)", (file))
-                conn.commit()
+cur.executemany("INSERT INTO tbl_textFiles (filename) VALUES (?)",
+                [(filename,) for filename in fileList if filename.endswith(".txt")])
+conn.commit()
 conn.close
-        
-conn = sqlite3.connect('104.db')
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM tbl_textFiles")
-    varFiles = cur.fetchall()
-    for item in varFiles:
-        msg = "ID: {} File Name: {}".format(item[0],item[1])
-    print(msg)
 
-conn.close
+for file in fileList:
+    if fnmatch.fnmatch(file, "*.txt"):
+        print(file)
+        
