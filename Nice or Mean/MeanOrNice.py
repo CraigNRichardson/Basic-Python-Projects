b5b5762adf3cb03a25f0280f163b47b532f989de
Craig Richardson <craig.n.richardson@gmail.com>
2020-06-10 21:45:40 -0600

    Create MeanOrNice.py
    Add Nice or Mean game file.

---
+++ Nice or Mean/MeanOrNice.py
@@ -0,0 +1,106 @@
+#
+# Python:   3.7.5
+#
+# Author:   Craig Richardson
+#
+# Purpose:  Python Course. Creating first program.
+#           Demonstrating how to pass variables from function to function
+#           while producing a functional game.
+#
+#           Remember, function_name(variable) _means that we pass in the variable.
+#           return variable _means that we are returning the variable back to
+#           the calling function.
+
+def start(nice=0,mean=0,name=""):
+    # get user's name
+    name = describe_game(name)
+    nice,mean,name = nice_mean(nice,mean,name)
+
+def describe_game(name):
+    """
+
+        check if this is a new game or not.
+        If it is new, get the user's name.
+        If it is not a new game, thank the player
+        for playing again and continue with the game
+    """
+    # meaning, if we do not already have this user's name,
+    # then they are a new player and we need to get their name.
+    if name != "":
+        print("\nThank you for playing again, {}".format(name))
+    else:
+        stop = True
+        while stop:
+            if name == "":
+                name = input("\nWhat is your name? \n>>> ").capitalize()
+                if name != "":
+                    print("\nWelcome, {}!".format(name))
+                    print("\nIn this game, you will be greeted\nby several different people. \nYou can choose to be nice or mean")
+                    print("but at the end of the game your fate\nwill be sealed by your actions.")
+                    stop = False
+    return name
+
+def nice_mean(nice,mean,name):
+    stop = True
+    while stop:
+        show_score(nice,mean,name)
+        pick = input("\nA Stranger approaches you for a \nconversation. Will you be nice \nor mean? (N/M) \n>>>: ").lower()
+        if pick == "n":
+            print("\nThe stranger walks away smiling...")
+            nice = (nice + 1)
+            stop = False
+        if pick == "m":
+            print("\nThe stranger glares at you \nmenacingly and storms off...")
+            mean = (mean + 1)
+            stop = False
+    score(nice,mean,name) # pass the 3 variables to the score()
+
+def show_score(nice,mean,name):
+    print("\n{}, your current total: \n({}, Nice) and ({}, Mean)".format(name,nice,mean))
+
+def score(nice,mean,name):
+    if nice > 2:
+        win(nice,mean,name)
+    if mean > 2:
+        lose(nice,mean,name)
+    else:
+        nice_mean(nice,mean,name)
+
+def win(nice,mean,name):
+    print("\nGreat job {}, you win! \nEveryone loves you and you've \nmade lots of friends along the way".format(name))
+    again(nice,mean,name)
+
+def lose(nice,mean,name):
+    print("\nYou lose, {}! \nYou've treated people like garbage, \nprobably because you are garbage yourself.".format(name))
+    again(nice,mean,name)
+
+
+def again(nice,mean,name):
+    stop = True
+    while stop:
+        choice = input("\nWould you like to play again? (y/n):\n>>> ").lower()
+        if choice == "y":
+            stop = False
+            reset(nice,mean,name)
+        if choice == "n":
+            print("\nOh, so sad, sorry to see you go!")
+            stop = False
+            quit()
+        else:
+            print("\nEnter ( Y ) for 'YES', ( N ) for 'NO':\n>>> ")
+        
+def reset(nice,mean,name):
+    nice = 0
+    mean = 0
+    start(nice,mean,name)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    start()
