84a7b91ba7a756d380e826c657c131198e4106e7
Craig Richardson <craig.n.richardson@gmail.com>
2020-07-08 09:30:27 -0600

    Create ChildClasses.py
    Add example of class inheritance.

---
+++ ChildClasses.py
@@ -0,0 +1,11 @@
+class FineArt:
+    artist = ' '
+    title = ' '
+    date = ' '
+    collection = ' '
+    location = ' '
+class Painting(FineArt):
+    medium = ' '
+class Photography(FineArt):
+    image_acquisition_method = ' '
+    print_type = ' '
